709d4966d1f12040a5e6c314c086ea9728aeace7
MITSUNARI Shigeo <herumi@nifty.com>
2017-11-07 11:27:30 +0900

    use assert

--- test.js
+++ test.js
@@ -1,16 +1,24 @@
 const she = require('./she.js')
+const assert = require('assert')
 
 she.init(() => {
-	let sec = new she.SecretKey()
-	sec.setByCSPRNG()
-	sec.dump('sec ')
-	let pub = sec.getPublicKey()
-	pub.dump('pub ')
-	let m1 = 9
-	let m2 = 5
-	let c1 = pub.encG1(m1)
-	let c2 = pub.encG2(m2)
-	let ct = she.mul(c1, c2)
-	console.log('dec ' + sec.dec(ct))
+  minimumTest()
 })
 
+function minimumTest() {
+  const sec = new she.SecretKey()
+  sec.setByCSPRNG()
+  const pub = sec.getPublicKey()
+  const m1 = 9
+  const m2 = 5
+  const m3 = 2
+  const m4 = -1
+  const c11 = pub.encG1(m1)
+  const c12 = pub.encG1(m2)
+  const c21 = pub.encG2(m3)
+  const c22 = pub.encG2(m4)
+  const c1 = she.add(c11, c12)
+  const c2 = she.add(c21, c22)
+  const ct = she.mul(c1, c2)
+  assert.equal(sec.dec(ct), (m1 + m2) * (m3 + m4))
+}
