ea1cb22983d3ce5eb1f3b0805391165a115712d2
Antoine Cormouls <contact.antoine.cormouls@gmail.com>
2020-11-19 10:05:55 +0100

    wip

---
+++ tests/BLS12_381.test.js
@@ -0,0 +1,4 @@
+const she = require('../she')
+const { curveTest } = require('./helper')
+
+curveTest(she.BLS12_381, 'BLS12_381')
---
+++ tests/BN381_1.test.js
@@ -0,0 +1,4 @@
+const she = require('../she')
+const { curveTest } = require('./helper')
+
+curveTest(she.BN381_1, 'BN381_1')
